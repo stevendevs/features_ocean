ef04ae2e2b3b6afc8623a97fb412749ee6350f2d
KevinAlpizarDev <kevinalpizardev@gmail.com>
2025-04-30 11:52:10 -0600

    Broadcast chat-message

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,10 @@
 class Message < ApplicationRecord
   belongs_to :chat
+after_create_commit :broadcast_new_message
+
+def broadcast_new_message
+broadcast_prepend_to(chat, :messages, target: "chat-message", particial: "messages/message", locals: { message: self})
+end
+
+
 end
